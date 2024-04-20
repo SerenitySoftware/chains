74fe150eb16ca90cb76c5e6f2234780ad77d019c
Jordan Ambra <jordan@serenity.software>
2024-04-20 18:58:14 -0400

    #2 Convert to unittest and improve coverage to 100%

--- chains/tests/test_iterables.py
+++ chains/tests/test_iterables.py
@@ -1,53 +1,62 @@
+import unittest
+
 from chains import Chain
 
 
-def test_dict_keys():
-    chainable = Chain({"key": "value", "nested": {"key": "value"}})
-    assert list(chainable.keys()) == ["key", "nested"]
-    assert list(chainable.nested.keys()) == ["key"]
-    assert "key" in chainable.keys()
-    assert "nested" in chainable
-    assert "missing" not in chainable
+class TestIterables(unittest.TestCase):
+    def test_dict_keys(self):
+        chainable = Chain({"key": "value", "nested": {"key": "value"}})
+        assert list(chainable.keys()) == ["key", "nested"]
+        assert list(chainable.nested.keys()) == ["key"]
+        assert "key" in chainable.keys()
+        assert "nested" in chainable
+        assert "missing" not in chainable
 
+    def test_lists(self):
+        chainable = Chain([1, 2, 3])
+        assert chainable[0] == 1
+        assert chainable[1] == 2
+        assert chainable[2] == 3
+        assert 1 in chainable
+        assert 4 not in chainable
+        assert 1 in chainable[0:2]
 
-def test_lists():
-    chainable = Chain([1, 2, 3])
-    assert chainable[0] == 1
-    assert chainable[1] == 2
-    assert chainable[2] == 3
-    assert 1 in chainable
-    assert 4 not in chainable
-    assert 1 in chainable[0:2]
+        assert sorted(chainable, reverse=True) == [3, 2, 1]
+        assert sorted(chainable, reverse=False) == [1, 2, 3]
+        assert sorted(chainable[0:2], reverse=True) == [2, 1]
+        assert sorted(chainable[0:2], reverse=False) == [1, 2]
 
-    assert sorted(chainable, reverse=True) == [3, 2, 1]
-    assert sorted(chainable, reverse=False) == [1, 2, 3]
-    assert sorted(chainable[0:2], reverse=True) == [2, 1]
-    assert sorted(chainable[0:2], reverse=False) == [1, 2]
+        for index, item in enumerate(chainable):
+            assert index + 1 == item
 
-    for index, item in enumerate(chainable):
-        assert index + 1 == item
+    def test_sets(self):
+        assert Chain(set()) == set()
+        assert Chain({1, 2, 3}) == {1, 2, 3}
+        assert 1 in Chain({1, 2, 3})
 
+    def test_strings(self):
+        chainable = Chain("abcdefghijklmnopqrstuvwxyz")
+        assert chainable[0] == "a"
+        assert chainable[1] == "b"
+        assert chainable[0:5] == "abcde"
 
-def test_sets():
-    assert Chain(set()) == set()
-    assert Chain({1, 2, 3}) == {1, 2, 3}
-    assert 1 in Chain({1, 2, 3})
+        assert "a" in chainable
+        assert "z" in chainable
+        assert "abc" in chainable
+        assert "xyz" in chainable
 
+    def test_generators(self):
+        chainable = Chain(range(5))
 
-def test_strings():
-    chainable = Chain("abcdefghijklmnopqrstuvwxyz")
-    assert chainable[0] == "a"
-    assert chainable[1] == "b"
-    assert chainable[0:5] == "abcde"
+        for index, item in enumerate(chainable):
+            assert index == item
 
-    assert "a" in chainable
-    assert "z" in chainable
-    assert "abc" in chainable
-    assert "xyz" in chainable
+    def test_non_iterable(self):
+        with self.assertRaises(TypeError):
+            enumerate(Chain(1))
 
+    def test_no_contains(self):
+        self.assertFalse("x" in Chain(None))
 
-def test_generators():
-    chainable = Chain(range(5))
-
-    for index, item in enumerate(chainable):
-        assert index == item
+    def test_no_len(self):
+        self.assertEqual(len(Chain(None)), 0)
--- chains/tests/test_operators.py
+++ chains/tests/test_operators.py
@@ -1,83 +1,82 @@
+import unittest
+
 from chains import Chain
 
 
-def test_add_number():
-    assert Chain(1) + 1 == 2
-    assert Chain(1) + 1.1 == 2.1
-    assert Chain(1.1) + 1 == 2.1
-    assert Chain(1.1) + 1.1 == 2.2
+class TestNavigation(unittest.TestCase):
 
+    def test_add_number(self):
+        assert Chain(1) + 1 == 2
+        assert Chain(1) + 1.1 == 2.1
+        assert Chain(1.1) + 1 == 2.1
+        assert Chain(1.1) + 1.1 == 2.2
+        assert Chain(None) + 10 == 10
 
-def test_add_string():
-    assert Chain("hello") + " world" == "hello world"
-    assert Chain("hello") + " " + Chain("world") == "hello world"
+    def test_add_string(self):
+        assert Chain("hello") + " world" == "hello world"
+        assert Chain("hello") + " " + Chain("world") == "hello world"
 
+    def test_add_list(self):
+        assert Chain([1, 2, 3]) + [4, 5, 6] == [1, 2, 3, 4, 5, 6]
+        assert Chain([1, 2, 3]) + Chain([4, 5, 6]) == [1, 2, 3, 4, 5, 6]
 
-def test_add_list():
-    assert Chain([1, 2, 3]) + [4, 5, 6] == [1, 2, 3, 4, 5, 6]
-    assert Chain([1, 2, 3]) + Chain([4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    def test_subtract_number(self):
+        assert Chain(1) - 1 == 0
+        assert Chain(0) - 1 == -1
+        assert Chain(2) - 1 == 1
+        assert Chain(1.1) - 1.1 == 0
+        assert Chain(10) - Chain(5) == 5
+        assert Chain(0) - Chain(10) == -10
+        assert Chain(None) - 10 == -10
 
+    def test_multiply_number(self):
+        assert Chain(1) * 1 == 1
+        assert Chain(1) * 2 == 2
+        assert Chain(2) * 2 == 4
+        assert Chain(5) * 10 == 50
+        assert Chain(5) * 0 == 0
+        assert Chain(5) * -1 == -5
+        assert Chain(5) * Chain(10) == 50
 
-def test_subtract_number():
-    assert Chain(1) - 1 == 0
-    assert Chain(0) - 1 == -1
-    assert Chain(2) - 1 == 1
-    assert Chain(1.1) - 1.1 == 0
-    assert Chain(10) - Chain(5) == 5
-    assert Chain(0) - Chain(10) == -10
+    def test_divide_number(self):
+        assert Chain(1) / 1 == 1
+        assert Chain(2) / 1 == 2
+        assert Chain(2) / 2 == 1
+        assert Chain(10) / 5 == 2
+        assert Chain(5) / 10 == 0.5
+        assert Chain(5) / 0 == 0
+        assert Chain(5) / -1 == -5
+        assert Chain(5) / Chain(10) == 0.5
 
+        # Yes Chain lets you divide by 0
+        assert Chain(10) / 0 == 0
+        assert Chain(10) / Chain(0) == 0
 
-def test_multiply_number():
-    assert Chain(1) * 1 == 1
-    assert Chain(1) * 2 == 2
-    assert Chain(2) * 2 == 4
-    assert Chain(5) * 10 == 50
-    assert Chain(5) * 0 == 0
-    assert Chain(5) * -1 == -5
-    assert Chain(5) * Chain(10) == 50
+    def test_modulo_number(self):
+        assert Chain(1) % 1 == 0
+        assert Chain(2) % 1 == 0
+        assert Chain(2) % 2 == 0
+        assert Chain(10) % 5 == 0
+        assert Chain(5) % 10 == 5
+        assert Chain(5) % 0 == 0
+        assert Chain(5) % -1 == 0
+        assert Chain(5) % Chain(10) == 5
 
+    def test_power_number(self):
+        assert Chain(1) ** 1 == 1
+        assert Chain(2) ** 1 == 2
+        assert Chain(2) ** 2 == 4
+        assert Chain(10) ** 5 == 100000
+        assert Chain(5) ** 0 == 1
+        assert Chain(5) ** -1 == 0.2
+        assert Chain(5) ** Chain(10) == 9765625
+        assert Chain(None) ** 10 == 0
 
-def test_divide_number():
-    assert Chain(1) / 1 == 1
-    assert Chain(2) / 1 == 2
-    assert Chain(2) / 2 == 1
-    assert Chain(10) / 5 == 2
-    assert Chain(5) / 10 == 0.5
-    assert Chain(5) / 0 == 0
-    assert Chain(5) / -1 == -5
-    assert Chain(5) / Chain(10) == 0.5
-
-    # Yes Chain lets you divide by 0
-    assert Chain(10) / 0 == 0
-    assert Chain(10) / Chain(0) == 0
-
-
-def test_modulo_number():
-    assert Chain(1) % 1 == 0
-    assert Chain(2) % 1 == 0
-    assert Chain(2) % 2 == 0
-    assert Chain(10) % 5 == 0
-    assert Chain(5) % 10 == 5
-    assert Chain(5) % 0 == 0
-    assert Chain(5) % -1 == 0
-    assert Chain(5) % Chain(10) == 5
-
-
-def test_power_number():
-    assert Chain(1) ** 1 == 1
-    assert Chain(2) ** 1 == 2
-    assert Chain(2) ** 2 == 4
-    assert Chain(10) ** 5 == 100000
-    assert Chain(5) ** 0 == 1
-    assert Chain(5) ** -1 == 0.2
-    assert Chain(5) ** Chain(10) == 9765625
-
-
-def test_floor_div():
-    assert Chain(1) // 1 == 1
-    assert Chain(2) // 1 == 2
-    assert Chain(2) // 2 == 1
-    assert Chain(17) // 5 == 3
-    assert Chain(5) // 10 == 0
-    assert Chain(4) // Chain(3) == 1
-    assert Chain(5) // 0 == 0
+    def test_floor_div(self):
+        assert Chain(1) // 1 == 1
+        assert Chain(2) // 1 == 2
+        assert Chain(2) // 2 == 1
+        assert Chain(17) // 5 == 3
+        assert Chain(5) // 10 == 0
+        assert Chain(4) // Chain(3) == 1
+        assert Chain(5) // 0 == 0
